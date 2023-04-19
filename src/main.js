6467a40f3b7260fe5cd4721d28f42969c3a2d971
alessiaBer <alexdeyn@gmail.com>
2023-04-19 22:37:17 +0200

    [import] fontawesome star icons

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import './assets/scss/app.scss'
 import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { faStar as farStar } from '@fortawesome/free-regular-svg-icons'
 import App from './App.vue'
 
-library.add(faStar)
-createApp(App).mount('#app')
+library.add(faStar, farStar) 
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
