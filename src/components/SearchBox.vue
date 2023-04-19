668ee845b6359bf859e05b987ff1454bda04fe4e
alessiaBer <alexdeyn@gmail.com>
2023-04-19 12:30:32 +0200

    [add] search box component

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,31 @@
+<script>
+import { store } from '../store';
+export default {
+    name: 'SearchBox',
+    emits: ['searchMovie'],
+    data() {
+        return {
+            store
+        }
+    }
+};
+</script>
+<template>
+    <div class="container">
+    <div class="input-group my-3">
+      <label class="input-group-text" for="search_input" @click="$emit('searchMovie')"
+        >Search
+      </label>
+      <input
+        type="text"
+        class="form-control"
+        id="search_input"
+        placeholder="Search film.."
+        @keyup="$emit('searchMovie')"
+        v-model="store.query"
+      />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
