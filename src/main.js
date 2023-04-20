a8724e63e1f1ed1a8386ef828d3abba3f64d2688
alessiaBer <alexdeyn@gmail.com>
2023-04-20 17:24:10 +0200

    [install]click-outside

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 import { faStar as farStar } from '@fortawesome/free-regular-svg-icons'
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import vueClickOutsideElement from 'vue-click-outside-element'
 import App from './App.vue'
 
 library.add(faStar, farStar, faMagnifyingGlass) 
-createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).use(vueClickOutsideElement).mount('#app')
