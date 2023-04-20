7b41fe5a456520ad6c76fee70b65f68ff0eff440
alessiaBer <alexdeyn@gmail.com>
2023-04-20 11:22:24 +0200

    [add] icon

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faStar } from '@fortawesome/free-solid-svg-icons'
 import { faStar as farStar } from '@fortawesome/free-regular-svg-icons'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 import App from './App.vue'
 
-library.add(faStar, farStar) 
+library.add(faStar, farStar, faMagnifyingGlass) 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
