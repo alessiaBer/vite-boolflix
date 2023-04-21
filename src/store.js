9e5ce7e75b7b3939eed93d7323bdd177edf3d929
alessiaBer <alexdeyn@gmail.com>
2023-04-21 17:30:54 +0200

    [add] comments

--- src/store.js
+++ src/store.js
@@ -5,8 +5,8 @@ export const store = reactive({
   MOVIEDB_API:
     "https://api.themoviedb.org/3/search/multi?api_key=892e430dec807d965a1a1412c9102c0a&query=",
   MOVIEIMG_URL: "https://image.tmdb.org/t/p/",
-  GETINFO_API: "https://api.themoviedb.org/3/",
-  GETGENRES_API: "https://api.themoviedb.org/3/genre/movie/list",
+  CREDITS_API: "https://api.themoviedb.org/3/",
+  GENRES_MOVIE: "https://api.themoviedb.org/3/genre/movie/list",
   GENRES_TV: "https://api.themoviedb.org/3/genre/tv/list",
   apiKey: "?api_key=892e430dec807d965a1a1412c9102c0a&",
   genresList: {
@@ -19,6 +19,11 @@ export const store = reactive({
   resultsList: [],
   movieVote: null,
   loading: true,
+
+  /**
+   * function to get movies/series
+   * @param {String} url 
+   */
   performSearchMovie(url) {
     axios
       .get(url)
@@ -34,16 +39,19 @@ export const store = reactive({
       });
   },
 
+  /**
+   * function to get the cast of the movie
+   * @param {String} mediaType 
+   * @param {Number} movieId 
+   */
   performGetInfo(mediaType, movieId) {
-    const credits_url = `${this.GETINFO_API + mediaType}/${movieId}/credits${
+    const credits_url = `${this.CREDITS_API + mediaType}/${movieId}/credits${
       this.apiKey
     }`;
     axios
       .get(credits_url)
       .then((response) => {
         this.entireCast = response.data.cast;
-        /* console.log(this.cast) */
-        /* for loop push in array 5 players */
         for (let i = 0; i < 5; i++) {
           const player = this.entireCast[i].name;
           this.showCast.push(player);
@@ -55,19 +63,17 @@ export const store = reactive({
     this.showCast = [];
   },
 
-
-
-
-
+  /** functions for multiple concurrent requests
+  * requesting movies genres list and tv genres list
+  */
   getTvGenres() {
     const tvGenresUrl = `${this.GENRES_TV + this.apiKey}`;
     return axios.get(tvGenresUrl)
   },
   getMovieGenres() {
-    const movieGenresUrl = `${this.GETGENRES_API + this.apiKey}`;
+    const movieGenresUrl = `${this.GENRES_MOVIE + this.apiKey}`;
     return axios.get(movieGenresUrl)
   },
-
   performGetGenres() {
     Promise.all([this.getTvGenres(), this.getMovieGenres()])
     .then(([tv, movie]) => {
