4fcaf7d0e412590a549d387f12105cbd96043da8
alessiaBer <alexdeyn@gmail.com>
2023-05-01 13:27:59 +0200

    [add] focusout

--- src/components/SearchBox.vue
+++ src/components/SearchBox.vue
@@ -1,28 +1,35 @@
 <script>
-import { store } from '../store';
+import { store } from "../store";
 export default {
-    name: 'SearchBox',
-    emits: ['searchMovie'],
-    data() {
-        return {
-            store,
-            showSearchBox: false
-        }
-    },
-    methods: {
-    }
+  name: "SearchBox",
+  emits: ["searchMovie"],
+  data() {
+    return {
+      store,
+      showSearchBox: false,
+    };
+  },
+  methods: {},
 };
 </script>
 <template>
   <div class="searchbox">
-    <font-awesome-icon icon="fa-solid fa-magnifying-glass" 
-    class="text-white glass-icon"
-    v-if="!showSearchBox"
-    @click="showSearchBox = true"/>
+    <font-awesome-icon
+      icon="fa-solid fa-magnifying-glass"
+      class="text-white glass-icon"
+      v-if="!showSearchBox"
+      @click="showSearchBox = true"
+    />
     <!--TODO CLICK-OUTSIDE-->
-    <div class="input-group" v-else>
-      <label class="input-group-text border-0 bg-transparent" for="search_input" @click="$emit('searchMovie')"
-        ><font-awesome-icon icon="fa-solid fa-magnifying-glass" class="text-white"/>
+    <div class="input-group" v-else @focusout="showSearchBox = false">
+      <label
+        class="input-group-text border-0 bg-transparent"
+        for="search_input"
+        @click="$emit('searchMovie')"
+        ><font-awesome-icon
+          icon="fa-solid fa-magnifying-glass"
+          class="text-white"
+        />
       </label>
       <input
         type="text"
@@ -30,7 +37,7 @@ export default {
         id="search_input"
         placeholder="Titles, people, genres"
         v-model="store.query"
-        @keyup="$emit('searchMovie')" 
+        @keyup="$emit('searchMovie')"
       />
     </div>
   </div>
